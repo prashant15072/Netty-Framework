c5a0c3ea8aa93e36de96c451e79cb649d443488d
prashant15072 <prashant15072@iiitd.ac.in>
2017-04-13 03:50:54 +0530

    First Server

---
+++ src/main/java/DiscardHandler.java
@@ -0,0 +1,29 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by PRASHANT on 4/12/2017.
+ */
+public class DiscardHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf a=(ByteBuf)msg;
+        try {
+            while (a.isReadable()){
+                System.out.println((char) a.readByte());
+                System.out.flush();
+            }
+        }
+        finally {
+            a.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/DiscardServer.java
@@ -0,0 +1,45 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by PRASHANT on 4/13/2017.
+ */
+public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port){
+        this.port=port;
+    }
+
+    public void run() throws Exception{
+        EventLoopGroup bossGroup=new NioEventLoopGroup();
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+        try {
+            ServerBootstrap b =new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new DiscardHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true);
+            ChannelFuture f=b.bind(port).sync();
+            f.channel().closeFuture().sync();
+
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        int port=8;
+        new DiscardServer(port).run();
+    }
+}
